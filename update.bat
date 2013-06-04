9584c1a24277b4d06564b4eeb0c6446306008e6a
Tomasz Janczuk <tomasz@janczuk.org>
2013-06-03 20:58:57 -0700

    fixes

--- update.bat
+++ update.bat
@@ -2,6 +2,6 @@
 setlocal ENABLEDELAYEDEXPANSION
 for /F %%I in ('dir /s /b .binpath') do (
     for /F %%J in (%%I) do robocopy %%J %%~dpI /MIR /XF .binpath /XF .gitignore
-    if !ERRORLEVEL! neq 0 echo ERROR & exit /b -1
+    if errorlevel 8 echo ERROR & exit /b -1
 )
 echo SUCCESS
